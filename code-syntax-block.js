97b8d42a2eda75aefc642be7f27fb75bdc1a5cda
Nathan Chere <git@nathanchere.com.au>
2019-05-06 22:56:14 +0200

    Remove more translations

--- code-syntax-block.js
+++ code-syntax-block.js
@@ -1,90 +1,89 @@
 /* global codeSyntaxBlockLanguages */
 
-const html = htm.bind( wp.element.createElement );
-const { __ } = wp.i18n;
+const html = htm.bind(wp.element.createElement);
 const { addFilter } = wp.hooks;
 const { PlainText, InspectorControls } = wp.editor;
 const { SelectControl } = wp.components;
 const { Fragment } = wp.element;
 
-const addSyntaxToCodeBlock = ( settings ) => {
-	if ( 'core/code' !== settings.name ) {
-		return settings;
-	}
+const addSyntaxToCodeBlock = (settings) => {
+    if ('core/code' !== settings.name) {
+        return settings;
+    }
 
-	return {
-		...settings,
+    return {
+        ...settings,
 
-		attributes: {
-			...settings.attributes,
-			language: {
-				type: 'string'
-			}
-		},
+        attributes: {
+            ...settings.attributes,
+            language: {
+                type: 'string'
+            }
+        },
 
-		edit({ attributes, setAttributes, isSelected, className }) {
-			const updateLanguage = language => {
-				setAttributes({ language });
-			};
+        edit({ attributes, setAttributes, isSelected, className }) {
+            const updateLanguage = language => {
+                setAttributes({ language });
+            };
 
-			// Note: Use of Fragment can be eliminated after https://github.com/developit/htm/issues/15.
-			return html`
+            // Note: Use of Fragment can be eliminated after https://github.com/developit/htm/issues/15.
+            return html `
 				<${Fragment}>
 					<${InspectorControls} key="controls">
 						<${SelectControl}
-							label=${ __( 'Language', 'code-syntax-block' ) }
-							value=${ attributes.language }
+							label=${ 'Language'}
+							value=${ attributes.language}
 							options=${
-								[
-									{ label: __( 'Auto-detect', 'code-syntax-block' ), value: '' },
-									...codeSyntaxBlockLanguages
-								]
-							}
-							onChange=${ updateLanguage }
+				[
+					{ label: 'Auto-detect', value: '' },
+					...codeSyntaxBlockLanguages
+				]
+				}
+							onChange=${ updateLanguage}
 						/>
 					</${InspectorControls}>
-					<div key="editor-wrapper" className=${ className }>
+					<div key="editor-wrapper" className=${ className}>
 						<${PlainText}
-							value=${ attributes.content }
-							onChange=${ ( content ) => setAttributes({ content }) }
-							placeholder=${ __( 'Write code…', 'code-syntax-block' ) }
-							aria-label=${ __( 'Code', 'code-syntax-block' ) }
+							value=${ attributes.content}
+							onChange=${ (content) => setAttributes({ content })}
+							placeholder=${ 'Write code…' }
+							aria-label=${ 'Code' }
 						/>
 					</div>
 				</${Fragment}>
 			`;
-		},
+        },
 
-		save({ attributes }) {
-			return html`
-				<pre><code>${ attributes.content }</code></pre>
+        save({ attributes }) {
+            return html `
+				<pre><code>${ attributes.content}</code></pre>
 			`;
-		},
+        },
 
-		// Automatically convert core code blocks to this new extended code block.
-		deprecated: [
-			...( settings.deprecated || []),
-			{
-				attributes: {
-					...settings.attributes,
-					language: {
-						type: 'string'
-					}
-				},
+        // Automatically convert core code blocks to this new extended code block.
+        deprecated: [
+            ...(settings.deprecated || []),
+            {
+                attributes: {
+                    ...settings.attributes,
+                    language: {
+                        type: 'string'
+                    }
+                },
 
-				save: function({ attributes }) {
-					const className = ( attributes.language ) ? 'language-' + attributes.language : '';
-					return html`
-						<pre><code lang=${ attributes.language } className=${ className }>${ attributes.content }</code></pre>
+                save: function({ attributes }) {
+                    const className = (attributes.language) ? 'language-' + attributes.language : '';
+                    return html `
+						<pre><code lang=${ attributes.language} className=${className}>${attributes.content}</code></pre>
 					`;
-				}
-			}
-		]
-	};
+                }
+            }
+        ]
+    };
 };
 
 addFilter(
-	'blocks.registerBlockType',
-	'westonruter/code-syntax-block',
-	addSyntaxToCodeBlock
+    'blocks.registerBlockType',
+    'nathanchere/nccomau-code-syntax-block',
+    addSyntaxToCodeBlock
 );
